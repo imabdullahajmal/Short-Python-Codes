0f6f69e72d1f1b8c47e0e4d02c5525ac76b71378
imabd <imabdullahajmal@gmail.com>
2023-10-05 00:53:38 +0500

    Created a python Program that generates random sentences

---
+++ random-sentences.py
@@ -0,0 +1,33 @@
+import random
+
+subj = ["He", "She", "They"]
+
+verbS = ["Plays", "Likes", "Hits", "Drives", "Punches", "Records"]
+verbP = ["Play", "Like", "Hit", "Drive", "Punch", "Record"]
+
+objL = ["Them", "Him", "Her"]
+objNL = ["Football", "Car", "House", "Boat", "Cricket", ""]
+
+obj = [random.choice(objL), random.choice(objNL)]
+
+noOfSent = int(input("How much sentences do you want ? :"))
+
+Sentence = ""
+
+def spacing(str1,str2,str3):
+    return str1+" " + str2 + " " + str3
+
+def generate_sentence():
+    s = random.choice(subj)
+
+    if s == 'They':
+        Sentence = spacing(s, random.choice(verbP), random.choice(obj))
+
+    else:
+        Sentence = spacing(s, random.choice(verbS), random.choice(obj))
+
+    return Sentence
+
+print("\n")
+for i in range(noOfSent):
+    print(generate_sentence())
